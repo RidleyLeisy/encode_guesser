385f3521c357c156a34b5583b43475cb9081a59b
RidleyLeisy <leisyridley@gmail.com>
2019-07-08 15:22:41 -0700

    updates:

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="encodeguesser",
+    name="encodeguess",
     version="0.0.1",
     author="Ridley Leisy",
     author_email="leisyridley@gmail.com",
